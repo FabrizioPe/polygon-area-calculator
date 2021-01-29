da5ad48cdf748af198286b4d4df1cd21d0cacc53
FabrizioPe <fab.pellegr@gmail.com>
2021-01-29 19:55:48 +0100

    Project finished. All tests passed.

--- README.md
+++ README.md
@@ -23,23 +23,23 @@ Additionally, the `set_width` and `set_height` methods on the Square class shoul
 
 #### Usage example
 ```py
-rect = shape_calculator.Rectangle(10, 5)
-print(rect.get_area())
-rect.set_height(3)
-print(rect.get_perimeter())
-print(rect)
-print(rect.get_picture())
-
-sq = shape_calculator.Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
-print(sq.get_picture())
-
-rect.set_height(8)
-rect.set_width(16)
-print(rect.get_amount_inside(sq))
+# rect = shape_calculator.Rectangle(10, 5)
+# print(rect.get_area())
+# rect.set_height(3)
+# print(rect.get_perimeter())
+# print(rect)
+# print(rect.get_picture())
+# 
+# sq = shape_calculator.Square(9)
+# print(sq.get_area())
+# sq.set_side(4)
+# print(sq.get_diagonal())
+# print(sq)
+# print(sq.get_picture())
+# 
+# rect.set_height(8)
+# rect.set_width(16)
+# print(rect.get_amount_inside(sq))
 ```
 That code should return:
 ```
--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,58 @@
 class Rectangle:
+    """A class representing a rectangle."""
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        if width > 0: self.width = width;
+        else: print("Error: side length must be positive.")
+
+    def set_height(self, height):
+        if height > 0: self.height = height;
+        else: print("Error: side length must be positive.")
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture(self):
+        """Return a graphical representation of the rectangle."""
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        return ('*' * self.width + '\n') * self.height
+
+    def get_amount_inside(self, shape):
+        """Calculate how many "shape"s fit inside the rectangle."""
+        return (self.width // shape.width) * (self.height // shape.height)
+
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
 
 
+class Square(Rectangle):
+    """A class representing a square, inheriting from Rectangle class."""
 
+    def __init__(self, side):
+        super().__init__(side, side)
 
-class Square:
+    def set_width(self, side):
+        super().set_width(side)
+        super().set_height(side)
+
+    def set_height(self, side):
+        super().set_height(side)
+        super().set_width(side)
+
+    def set_side(self, side):
+        super().set_height(side)
+        super().set_width(side)
+
+    def __str__(self):
+        return f"Square(side={self.width})"
